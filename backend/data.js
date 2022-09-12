984f93eb26a686d87766d0793fc5d99d0a79cd24
Biplob Molla <ahmedtamim317@gmail.com>
2022-09-12 17:55:49 +0600

    Commit

---
+++ backend/data.js
@@ -0,0 +1,54 @@
+const data = {
+    products: [
+        {
+            name: 'Nike Slim Shirts',
+            slug: 'nike-slim-shirts',
+            category: 'Shirts',
+            image: '/images/p1.jpg',
+            price: 120,
+            countInStock: 0,
+            brand: 'Nike',
+            rating: 2,
+            numReviews: 10,
+            description: 'High quality shirt'
+        },
+        {
+            name: 'Nike Slim Pants',
+            slug: 'nike-slim-pants',
+            category: 'Pants',
+            image: '/images/p2.jpg',
+            price: 150,
+            countInStock: 7,
+            brand: 'Nike',
+            rating: 3,
+            numReviews: 15,
+            description: 'High quality pant'
+        },
+        {
+            name: 'Nike Slim Shoes',
+            slug: 'nike-slim-shoes',
+            category: 'Shoes',
+            image: '/images/p3.jpg',
+            price: 110,
+            countInStock: 20,
+            brand: 'Nike',
+            rating: 4,
+            numReviews: 15,
+            description: 'High quality shoes'
+        },
+        {
+            name: 'Adidas Slim Shirts',
+            slug: 'adidas-slim-shirts',
+            category: 'Shirts',
+            image: '/images/p5.jpg',
+            price: 120,
+            countInStock: 10,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'High quality shirt'
+        }
+    ]
+}
+
+export default data;
