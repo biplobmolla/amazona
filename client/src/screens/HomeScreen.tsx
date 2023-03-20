523b401112eebf4f5a5eb06705a29535f96b34bd
Mohammad Biplob Molla <69385735+biplobmolla@users.noreply.github.com>
2023-03-20 13:29:32 +0600

    Revert "Update HomeScreen.tsx"
    This reverts commit 30d787d56b1f3379782cbd62e1901ab09ca9d75d.

--- client/src/screens/HomeScreen.tsx
+++ client/src/screens/HomeScreen.tsx
@@ -1,36 +1,13 @@
 import axios from "axios";
-import { useEffect, useReducer, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-const reducer = (state: any, action: any) => {
-  switch (action.type) {
-    case "FETCH_REQUEST":
-      return { ...state, loading: true };
-    case "FETCH_SUCCESS":
-      return { ...state, products: action.payload, loading: false };
-    case "FETCH_FAIL":
-      return { ...state, loading: false, error: action.payload };
-    default:
-      return state;
-  }
-};
-
 function HomeScreen() {
-  const [{ loading, products, error }, dispatch] = useReducer(reducer, {
-    products: [],
-    loading: true,
-    error: "",
-  });
-
+  const [products, setProducts] = useState([]);
   useEffect(() => {
     const fetchProducts = async () => {
-      dispatch({ type: "FETCH_REQUEST" });
-      try {
-        const result = await axios.get("/api/products");
-        dispatch({ type: "FETCH_SUCCESS", payload: result.data });
-      } catch (error: any) {
-        dispatch({ type: "FETCH_FAIL", error: error.message });
-      }
+      const result = await axios.get("/api/products");
+      setProducts(result.data);
     };
     fetchProducts();
   }, []);
@@ -38,28 +15,22 @@ function HomeScreen() {
     <div>
       <h1>Featured Products</h1>
       <div className="products">
-        {loading ? (
-          <div>Loading...</div>
-        ) : error ? (
-          <div>{error}</div>
-        ) : (
-          products?.map((product: any) => (
-            <div key={product?.slug} className="product">
-              <Link to={`/product/${product?.slug}`}>
-                <img src={product?.image} alt={product?.name} />
+        {products?.map((product: any) => (
+          <div key={product?.slug} className="product">
+            <Link to={`/product/${product?.slug}`}>
+              <img src={product?.image} alt={product?.name} />
+            </Link>
+            <div className="product-info">
+              <Link to={`/product/${product.slug}`}>
+                <p>{product.name}</p>
               </Link>
-              <div className="product-info">
-                <Link to={`/product/${product.slug}`}>
-                  <p>{product.name}</p>
-                </Link>
-                <p>
-                  <strong>${product.price}</strong>
-                </p>
-                <button>Add to cart</button>
-              </div>
+              <p>
+                <strong>${product.price}</strong>
+              </p>
+              <button>Add to cart</button>
             </div>
-          ))
-        )}
+          </div>
+        ))}
       </div>
     </div>
   );
