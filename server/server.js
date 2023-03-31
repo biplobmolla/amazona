cdd4ce9c842b29ede567ae2f4a9ac988e4bee6b1
Mohammad Biplob Molla <69385735+biplobmolla@users.noreply.github.com>
2023-03-31 14:05:03 +0600

    Revert "Till Fetching Data From Database"
    This reverts commit 658e43302d49cf1bc77818b073115a43ab96e48f.

--- server/server.js
+++ server/server.js
@@ -1,8 +1,6 @@
 import express from "express";
 import data from "./data.js";
 import mongoose from "mongoose";
-import seedRouter from "./routes/seedRoutes.js";
-import productRouter from "./routes/productRoutes.js";
 
 const app = express();
 app.use(express.json());
@@ -12,8 +10,29 @@ mongoose
   .then(() => console.log("Connected to db"))
   .catch((err) => console.log("Error: ", err.message));
 
-app.use("/api/seed", seedRouter);
-app.use("/api/products/", productRouter);
+app.get("/api/products/", (req, res) => {
+  res.send(data.products);
+});
+
+app.get("/api/products/product/:slug", (req, res) => {
+  const product = data.products.find((x) => x.slug === req.params.slug);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product Not Found" });
+  }
+  res.send(data.products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: "Product Not Found" });
+  }
+  res.send(data.products);
+});
 
 const port = 5000;
 
