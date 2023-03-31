e8741c00eb1b1cc4e8454a7947727c0607d5ab17
Mohammad Biplob Molla <69385735+biplobmolla@users.noreply.github.com>
2023-03-31 16:52:23 +0600

    Till JWT Token Get

--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
-import express from "express";
+import express, { urlencoded } from "express";
 import mongoose from "mongoose";
 import productRouter from "./routes/productRoutes.js";
 import seedRouter from "./routes/seedRoutes.js";
+import userRouter from "./routes/userRoutes.js";
 
 mongoose
   .connect("mongodb://127.0.0.1:27017/amazona")
@@ -10,9 +11,14 @@ mongoose
 
 const app = express();
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/seed", seedRouter);
 app.use("/api/products/", productRouter);
+app.use("/api/users/", userRouter);
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 const port = 5000;
 
